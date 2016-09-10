ca1f48fb84032f6e421d7a3ba9c1bec6faa11da7
Duyb <1255248666@qq.com>
2016-09-10 10:57:52 +0800

    express

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Du', signature: '白马山庄', version: 2 });
+  res.render('home', { title: 'Du', signature: '白马山庄', version: 2 });
 });
 
 module.exports = router;
