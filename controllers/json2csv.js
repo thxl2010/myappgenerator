51ca11d10f2fd06b0b1407f465100069c95bb453
Duyb <1255248666@qq.com>
2020-04-08 23:55:54 +0800

    add: spider canada.ca glossary

---
+++ controllers/json2csv.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+const { parse } = require('json2csv');
+const fields = ['letter', 'key', 'content'];
+const opts = { fields };
+
+const GLOSSARY_MAP = require('./exports/canada-ca-glossary.json');
+// console.log(JSON.stringify(GLOSSARY_MAP));
+
+function append2Csv(path, value) {
+  console.log(path, value);
+  fs.appendFile(path, value, 'utf8', (err) => {
+    if (err) {
+      // console.log('csv 写入失败', err);
+    } else {
+      // console.log('csv 写入成功');
+    }
+  });
+}
+
+function json2Csv(name, data) {
+  try {
+    const csv = parse(data, opts);
+    // console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^ json2Csv name : ${name} , csv : ${csv}`);
+    append2Csv(`./exports/${name}-glossary.csv`, csv);
+  } catch (err) {
+    // console.error(`^^^^^^^^^^^^^^^^^^^^^^^  json2Csv err: ${err}`);
+  }
+}
+
+function add() {
+  arr = [];
+  Object.entries(GLOSSARY_MAP).forEach((item, i) => {
+    // append2Json(item[0], item[1]);
+    // console.log(`\n >>>>>>>>>>>> \n ${i} item[1]`, JSON.stringify(item[1]));
+    json2Csv('canada-ca', item[1]);
+  });
+}
+
+add();
